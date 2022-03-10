40ae7c2d33799e671fba64ba678d852c817541c8
YuweiChang <ywchangsmail@gmail.com>
2022-03-10 20:27:37 +0800

    /cats & /cat/:id mocks

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,36 +1,41 @@
 import { rest } from 'msw'
 
 export const handlers = [
-  rest.post('/cats', (req, res, ctx) => {
+  rest.get('/cat/:id', (req, res, ctx) => {
     return res(
-      ctx.status(200),
-      ctx.json([
-        {
-          id: '595f280a557291a9750ebf58',
-          created_at: '2016-12-28T11:51:30.730Z',
-          tags: ['advent-calendar-2016', 'gif']
-        },
-        {
-          id: '595f280a557291a9750ebf62',
-          created_at: '2016-09-22T19:20:55.627Z',
-          tags: ['jump', 'fail', 'gif']
-        },
-        {
-          id: '595f280b557291a9750ebf67',
-          created_at: '2016-09-22T19:22:49.206Z',
-          tags: ['boxe', 'gif']
-        },
-        {
-          id: '595f280b557291a9750ebf6c',
-          created_at: '2016-09-22T19:12:54.037Z',
-          tags: ['fail', 'roll', 'gif']
-        },
-        {
-          id: '595f280c557291a9750ebf76',
-          created_at: '2016-11-25T03:46:11.837Z',
-          tags: ['kitten']
-        }
-      ])
+      ctx.status(200), 
+      ctx.json(catsMock.find((cat) => cat.id === req.params.id))
     )
+  }),
+  rest.post('/cats', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(catsMock))
   })
+]
+
+const catsMock = [
+  {
+    id: '595f280a557291a9750ebf58',
+    created_at: '2016-12-28T11:51:30.730Z',
+    tags: ['advent-calendar-2016', 'gif']
+  },
+  {
+    id: '595f280a557291a9750ebf62',
+    created_at: '2016-09-22T19:20:55.627Z',
+    tags: ['jump', 'fail', 'gif']
+  },
+  {
+    id: '595f280b557291a9750ebf67',
+    created_at: '2016-09-22T19:22:49.206Z',
+    tags: ['boxe', 'gif']
+  },
+  {
+    id: '595f280b557291a9750ebf6c',
+    created_at: '2016-09-22T19:12:54.037Z',
+    tags: ['fail', 'roll', 'gif']
+  },
+  {
+    id: '595f280c557291a9750ebf76',
+    created_at: '2016-11-25T03:46:11.837Z',
+    tags: ['kitten']
+  }
 ]
