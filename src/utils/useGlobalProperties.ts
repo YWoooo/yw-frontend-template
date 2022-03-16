12c8dc7aeef9e5503d005d10d6a5a3b20762e40b
YuweiChang <ywchangsmail@gmail.com>
2022-03-16 23:01:44 +0800

    remove useGlobalProperties - since it's not really fit with VTU - also, Evan Yu also suggest just import things we need

--- src/utils/useGlobalProperties.ts
+++
@@ -1,8 +0,0 @@
-import { getCurrentInstance } from 'vue';
-import { ComponentCustomProperties } from '@vue/runtime-core';
-
-export default function useGlobalProperties () {
-  const app = getCurrentInstance();
-
-  return app!.appContext.config.globalProperties as ComponentCustomProperties;
-}
