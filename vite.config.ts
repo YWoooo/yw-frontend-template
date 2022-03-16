69343a1e23e20f77d0147b0465bc24c1cfd50fcb
YuweiChang <ywchangsmail@gmail.com>
2022-03-16 22:54:43 +0800

    remove unplugin-auto-import & unplugin-vue-components - they're not really fit with Jest & VTU - It's such a pity that seems like Anthony doesn't really into this issue: https://github.com/antfu/unplugin-auto-import/issues/33

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
 import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    WindiCSS(),
-    AutoImport({
-      include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/, /\.md$/],
-      imports: ['vue', 'vue-router']
-    }),
-    Components({
-      extensions: ['vue', 'md'],
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      dts: 'src/components.d.ts'
-    })
+    WindiCSS()
   ],
   resolve: {
     alias: {
